9913f104a892eb225a862641271d2a0be07abc62
liangwenhan <2063957519@qq.com>
2016-10-08 13:06:19 +0800

    test1

--- LeetCodeAlgorithms/src/leetcode/Largest_Rectangle_in_Histogram.java
+++ LeetCodeAlgorithms/src/leetcode/Largest_Rectangle_in_Histogram.java
@@ -3,7 +3,7 @@ package leetcode;
 /**
  * Given n non-negative integers representing the histogram's 
  * bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
- * @author hasee
+ * @author lwh
  *
  */
 public class Largest_Rectangle_in_Histogram {
