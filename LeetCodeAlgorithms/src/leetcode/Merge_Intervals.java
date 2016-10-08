ccbadfdcf8644b8b97718319866c548bfcdace86
kitty <13534085364@163.com>
2016-10-08 14:30:40 +0800

    2

--- LeetCodeAlgorithms/src/leetcode/Merge_Intervals.java
+++ LeetCodeAlgorithms/src/leetcode/Merge_Intervals.java
@@ -18,6 +18,7 @@ import java.util.List;
  * public class Interval {
  *     int start;
  *     int end;
+ *     
  *     Interval() { start = 0; end = 0; }
  *     Interval(int s, int e) { start = s; end = e; }
  * }
